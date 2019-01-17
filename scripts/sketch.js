5d833f3e099b3d74a2d0e9de87d961f290b39724
Rafael Caro Repetto <rafael.caro.repetto@gmail.com>
2019-01-17 12:01:42 +0100

    First version of working sadja button

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -5,10 +5,13 @@ var backColor;
 
 var soundsInfo;
 var sounds = [];
+var sadjaList = [];
 var currentSound;
+var sadja;
 
 var buttonSearchSound;
 var buttonPlay;
+var buttonSadja;
 
 function preload () {
   soundsInfo = loadJSON("files/soundsInfo.json");
@@ -20,15 +23,24 @@ function setup () {
   div.style("width: " + width + "px; margin: 10px auto; position: relative;");
   canvas.parent("sketch-holder");
 
+  sadja = new p5.Oscillator();
+
   buttonSearchSound = createButton("Comienza")
     .size(75, 25)
     .position(extraSpaceW + 15, extraSpaceH + 15)
-    .mousePressed(soundLoader)
+    .mouseClicked(soundLoader)
     .parent("sketch-holder");
   buttonPlay = createButton("Toca")
     .size(75, 25)
     .position(extraSpaceW + 15, buttonSearchSound.y + 30)
-    .mousePressed(player)
+    .mouseClicked(player)
+    .attribute("disabled", "true")
+    .parent("sketch-holder");
+  buttonSadja = createButton("Ṣaḍja")
+    .size(75, 25)
+    .position(extraSpaceW + 15, buttonPlay.y + 30)
+    .mousePressed(function () {sadja.start();})
+    .attribute("onmouseup", "sadja.stop()")
     .attribute("disabled", "true")
     .parent("sketch-holder");
 
@@ -56,15 +68,18 @@ function soundLoader () {
   if (sounds.length == 0) {
     var start = millis();
     for (var i = 0; i < Object.keys(soundsInfo).length; i++) {
-      sounds[i] = loadSound("sounds/" + soundsInfo[str(i)].fileName, print("Loaded " + str(i)));
+      sounds[i] = loadSound("sounds/" + soundsInfo[str(i)].fileName);
+      sadjaList[i] = soundsInfo[str(i)].sa;
     }
     var timeLapse = millis() - start;
     print("All sounds loaded in " + str(timeLapse) + " seconds.");
     var index = int(random(Object.keys(soundsInfo).length));
     currentSound = sounds[index];
     print(index, currentSound);
+    sadja.freq(sadjaList[index]);
     buttonSearchSound.html("Re-afina");
     buttonPlay.removeAttribute("disabled");
+    buttonSadja.removeAttribute("disabled");
   } else {
     if (currentSound.isPlaying()) {
       currentSound.stop();
@@ -72,6 +87,7 @@ function soundLoader () {
     }
     var index = int(random(Object.keys(soundsInfo).length));
     currentSound = sounds[index];
+    sadja.freq(sadjaList[index]);
     print(index, currentSound);
   }
 }
